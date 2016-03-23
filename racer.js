e511391de542b250cfbee1fc249fc0002b1d1ba1
Lauren Colls <lauren.colls@hotmail.com>
2016-03-23 21:31:06 +1300

    add keyup event listener

--- racer.js
+++ racer.js
@@ -1,3 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
-  //run the code
+  console.log("document ready")
+
+
+
+var player1Strip = document.querySelectorAll('#player1_strip td')
+var player2Strip = document.querySelectorAll('#player2_strip td')
+document.addEventListener('keyup', updatePlayerPosition);
+
+
+
+
+
+function updatePlayerPosition(player){
+
+if (event.which === 90) {
+	console.log(event.which)
+	var player = player1Strip 
+
+} else if (event.which === 77) {
+	var player = player2Strip
+}
+
+for (var i = 1; i < player.length; i++) {
+	if 	(player[i].className === "active") {
+	player[i].className = ""
+
+
+	} else {
+player[i].className += "active"
+		
+	}
+
+
+
+break
+
+}
+
+
+}
+
 })
