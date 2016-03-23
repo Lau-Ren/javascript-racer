cace9181ab48e061abcd24cb112510034c4be332
Lauren Colls <lauren.colls@hotmail.com>
2016-03-24 11:02:15 +1300

    add restart game function

--- racer.js
+++ racer.js
@@ -1,52 +1,67 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log("document ready")
-
-
+var restart= false
 var position1 =1
 var position2 =1;
 var player1Strip = document.querySelectorAll('#player1_strip td')
 var player2Strip = document.querySelectorAll('#player2_strip td')
-document.addEventListener('keyup', updatePlayerPosition);
+var play = document.addEventListener('keyup', updatePlayerPosition);
+
+	function updatePlayerPosition(){
+
+		if (event.which === 90) {
+			if (player1Strip[position1].className ===""){
+				console.log(player1Strip[position1].className)
+				player1Strip[position1].className = "active"
+				player1Strip[position1-1].className = ""
+				position1 += 1
+			} else if (player1Strip[position1].className === "finish") {
+				console.log(player1Strip[position1].className)
+				// player1Strip[position1].className = "active"
+				player1Strip[position1-1].className = ""
+				alert("congratulations Player 1, you are the winner!")
+				restart = confirm("play again?")
+				if (restart) {
+					restartGame()
+
+				}
+			}
+
+		} else if (event.which === 77) {
+			if (player2Strip[position2].className ===""){
+				console.log(player2Strip[position2].className)
+				player2Strip[position2].className = "active"
+				player2Strip[position2-1].className = ""
+				position2 += 1
+			} else if (player1Strip[position2].className === "finish") {
+				console.log(player2Strip[position2].className)
+				// player1Strip[position1].className = "active"
+				player2Strip[position2-1].className = ""
+				alert("congratulations Player 2, you are the winner!")
+				restart = confirm("play again?")
+				if (restart) {
+					restartGame()
+
+				}
+			}
+		}
 
 
+	}
 
 
+	function restartGame() {
+		position1 =1
+		position2 =1
 
-function updatePlayerPosition(player){
-
-if (event.which === 90) {
-	
-	console.log(event.which)
-	player1Strip[position1].className = "active"
-	position1 += 1
-
-} else if (event.which === 77) {
-
-	
-	player2Strip[position2].className = "active"
-	position2 += 1
-}
+		for (var i = 1; i < player1Strip.length; i++ ){
+		player1Strip[position1].className = ""
+		player2Strip[position2].className = ""	
+		player1Strip[0].className = "active"	
+		player2Strip[0].className = "active"	
+		restart = false
+		}
 
 
-
-
-// for (var i = 1; i < player.length; i++) {
-// 	if 	(player[i].className === "active") {
-// 	player[i].className = ""
-
-
-// 	} else {
-// player[i].className += "active"
-
-// 	}
-
-
-
-// break
-
-// }
-
-
-}
+	}
 
 })
