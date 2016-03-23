9f9bb113cf0f408e59d1fca802234c633c0d3ec6
Lauren Colls <lauren.colls@hotmail.com>
2016-03-24 09:32:39 +1300

    make players move indp along own strip

--- racer.js
+++ racer.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   console.log("document ready")
 
 
-var position = 1
+var position1 =1
+var position2 =1;
 var player1Strip = document.querySelectorAll('#player1_strip td')
 var player2Strip = document.querySelectorAll('#player2_strip td')
 document.addEventListener('keyup', updatePlayerPosition);
@@ -14,15 +15,19 @@ document.addEventListener('keyup', updatePlayerPosition);
 function updatePlayerPosition(player){
 
 if (event.which === 90) {
+	
 	console.log(event.which)
-	var player = player1Strip 
+	player1Strip[position1].className = "active"
+	position1 += 1
 
 } else if (event.which === 77) {
-	var player = player2Strip
+
+	
+	player2Strip[position2].className = "active"
+	position2 += 1
 }
 
-player[position].className = "active"
-position += 1
+
 
 
 // for (var i = 1; i < player.length; i++) {
