1dfd2143f62918de87444e639e8b8aa6dbefda51
Lauren Colls <lauren.colls@hotmail.com>
2016-03-25 13:53:30 +1300

    refactor updatePlayerPosition function

--- racer.js
+++ racer.js
@@ -1,76 +1,71 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 var restart= false
+var position
 var position1 =1
 var position2 =1;
-var play = document.addEventListener('keyup', updatePlayerPosition);
+var play = document.addEventListener('keyup', selectPlayer);
 var lengthOfTrack  = 5 
 var strips = document.querySelectorAll('tr')
-var player1Strip = document.querySelectorAll('#player1_strip td')
-var player2Strip = document.querySelectorAll('#player2_strip td')
+var playerStrip
 
 lengthOfTrack = prompt("length of race course?") 
+	// create length of racecourse:
+	// loops for the number of player strips
+	for (var j = 0; j < strips.length; j++) { 
+		// loop for the number length of course	
+		for ( var k = 0; k < lengthOfTrack; k++){
+			var square = document.createElement('td')
+			strips[j].appendChild(square)
+		}
+		// sets last cell in array as the finish line
+		square.className = "finish"
+	}
 
-		 // create length of racecourse:
-			// loops for the number of player strips
-			for (var j = 0; j < strips.length; j++) { 
-				// loop for the number length of course	
-				for ( var k = 0; k < lengthOfTrack; k++){
-					var square = document.createElement('td')
-					strips[j].appendChild(square)
-				}
-				// sets last cell in array as the finish line
-				square.className = "finish"
-			}
+	restartGame()
+	// move player 1 position every keyup event
+	// check which key 
 
-
-
-restartGame()
-	// called for every keyup event
-	function updatePlayerPosition(){
-		// if key up event is z/player 1
+	function selectPlayer(){
+		var player 
 		if (event.which === 90) {
-			// during race active class is added to table cell player is on, position is incremented by 1
-			if (player1Strip[position1].className ===""){
-				
-				player1Strip[position1].className = "active"
-				player1Strip[position1-1].className = ""
-				position1 += 1
-			// when player reaches the end position is not incremented, given option to play again.
-			} else if (player1Strip[position1].className === "finish") {
-				
-				player1Strip[position1-1].className = ""
-				alert("Congratulations Moose, you are the winner! Please enjoy your kale smoothie")
-				restart = confirm("play again?")
-				// if player doesnt restart game then event listener for key up is removed
-				if (restart) {
-					restartGame()
-				}else {
-					play = document.removeEventListener('keyup', updatePlayerPosition);
-				}
-			}
+			playerStrip= document.querySelectorAll('#player1_strip td')
+			position = position1
 		// if key up event is m/player 2
 		} else if (event.which === 77) {
+			playerStrip = document.querySelectorAll('#player2_strip td')
+			position = position2
+		}
+		updatePlayerPosition(playerStrip, position)
+	}
 
-			if (player2Strip[position2].className ===""){
-				
-				console.log(player2Strip[position2].className)
-				player2Strip[position2].className = "active"
-				player2Strip[position2-1].className = ""
-				position2 += 1
-
-			} else if (player1Strip[position2].className === "finish") {
-				
-				console.log(player2Strip[position2].className)
-				player2Strip[position2-1].className = ""
-				alert("Congratulations Kangaroo, you are the winner! Please enjoy your kale smoothie")
-				restart = confirm("play again?")
-
-				if (restart) {
-					restartGame()
-				}else {
-					play = document.removeEventListener('keyup', updatePlayerPosition);
-				}
+	function updatePlayerPosition(strip, position){
+		
+		// add active class to current cell, remove active class from previous cell.
+		// position is incremented by 1
+		if (strip[position].className ===""){
+			
+			strip[position].className = "active"
+			strip[position-1].className = ""
+			position += 1
+			if (event.which ===90){
+				position1 = position
+				playerAnimal = "moose"
+			} else if (event.which ===77){
+				position2 = position
+				playerAnimal = "kangaroo"
+			}
+		// when player reaches the end position is not incremented, given option to play again.
+		} else if (strip[position].className === "finish") {
+			
+			strip[position-1].className = ""
+			alert("Congratulations " + playerAnimal + ", you are the winner! Please enjoy your kale smoothie")
+			restart = confirm("play again?")
+			// if player doesnt restart game then event listener for key up is removed
+			if (restart) {
+				restartGame()
+			}else {
+				play = document.removeEventListener('keyup', updatePlayerPosition);
 			}
 		}
 	}
